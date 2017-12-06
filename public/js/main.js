45f637ee1317c30bc116b876ff5c9f575268f2a7
_vasilie <vasilije.milenkovic@diwanee.com>
2017-12-06 16:39:22 +0100

    resultas started

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,26 @@
 // PROCHAINS SLIDER
 $(".b-prochains__slider").slick({
   slidesToShow:3,
+  responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 640,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+]
 });
+$(".b-resultats__tags").slick({
+  slidesToShow:5,
+})
+$(".b-resultats__dates").slick({
+  slidesToShow:1,
+})
